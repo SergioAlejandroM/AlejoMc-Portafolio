f084614db703118b8cc637da681911ce766a5b42
AlejoMc <alejocoder03@gmail.com>
2024-05-30 13:16:23 -0500

    Carga de la seccion home (css html) y scrollbar (js)

--- assets/JS/Main.js
+++ assets/JS/Main.js
@@ -7,3 +7,17 @@ function myMenuFunction(){
         menuBtn.className = "nav-menu";
     }
 }
+
+window.onscroll = function() {headerShadow()};
+    function headerShadow() {
+      const navHeader =document.getElementById("header");
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+      } else {
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+      }
+    }
